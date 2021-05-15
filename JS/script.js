55eb41e64e575d2050f36a4269bf32cfd6c10ef0
KTnomis <70838982+KTnomis@users.noreply.github.com>
2021-05-15 21:01:33 +0700

    yes
    uh

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,8 @@
-$(document).ready(function() {
-    var $input = $('.form-input');
-
-    $input.focusout(function() {
-        if($(this).val().length > 0) {
-            $(this).addClass('input-focus');
-            $(this).next('.form-label').addClass('input-focus-label');
-        }
-        else {
-        $(this).removeClass('input-focus');
-            $(this).next('.form-label').removeClass('input-focus-label');
-            
-        }
-    });
-});
+function hidepassword() {
+    var x = document.getElementById("pass");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
